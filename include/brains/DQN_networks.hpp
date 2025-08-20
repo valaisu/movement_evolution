da87c7626eab12ec69c67cacf8681bb4b439b492
valaisu <hallman.valo@gmail.com>
2025-08-20 18:36:30 +0300

    Add torch library to cmake

--- include/brains/DQN_networks.hpp
+++ include/brains/DQN_networks.hpp
@@ -3,3 +3,24 @@
 // Q - netowork
 
 // T -network
+/*
+#include <torch/torch.h>
+
+// Define a simple neural network
+struct QNet : torch::nn::Module {
+  QNet() {
+    fc1 = register_module("fc1", torch::nn::Linear(512, 256));
+    fc2 = register_module("fc2", torch::nn::Linear(256, 128));
+    fc3 = register_module("fc2", torch::nn::Linear(128, 64));
+  }
+  
+  torch::Tensor forward(torch::Tensor x) {
+    x = torch::relu(fc1->forward(x));
+    x = torch::relu(fc2->forward(x));
+    x = fc3->forward(x);
+    return x;
+  }
+  
+  torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+};
+*/
